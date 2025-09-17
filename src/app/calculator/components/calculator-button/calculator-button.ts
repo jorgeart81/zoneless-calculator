71ebdb10ba887ff6b749937b3abaf212b7e96a26
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-09-17 00:37:05 -0500

    feat(CalculatorButton): Add keyboardPressedStyle

--- src/app/calculator/components/calculator-button/calculator-button.ts
+++ src/app/calculator/components/calculator-button/calculator-button.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, output, signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -6,23 +6,49 @@ import { Component, HostBinding, input } from '@angular/core';
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400',
     attribute: 'key-button',
+    '[class.is-command]': 'isCommand()',
+    '[class.is-double-size]': 'isDoubleSize()',
+    '[class.is-pressed]': 'isPressed()',
   }
 })
-export class CalculatorButton {
+export class CalculatorButton implements AfterViewInit {
+  public isPressed = signal(false)
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+  private button: HTMLButtonElement | undefined
+  private timer: ReturnType<typeof setTimeout> | undefined = undefined;
+
+  ngAfterViewInit() {
+    this.button = this.contentValue()?.nativeElement
+  }
+
   public isCommand = input(false, {
     transform: (value: string | boolean) => typeof value === 'string' ? value === '' : value
   });
-
   public isDoubleSize = input(false, {
     transform: (value: boolean | string) =>
       typeof value === 'string' ? value === '' : value,
   });
 
-  @HostBinding('class.is-command') get bgIndigo500() {
-    return this.isCommand();
+  handleClick() {
+    if (!this.button) return
+    this.onClick.emit(this.button.innerText.trim());
   }
 
-  @HostBinding('class.is-double-size') get commandStyle() {
-    return this.isDoubleSize();
+  public keyboardPressedStyle(key: string) {
+    if (!this.button) return
+    const value = this.button.innerText.trim()
+
+    if (key.trim() !== value) return
+
+    this.isPressed.set(true)
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+
+    this.timer = setTimeout(() => {
+      this.isPressed.set(false)
+    }, 100);
   }
+
 }
