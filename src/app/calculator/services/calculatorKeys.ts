fe9be6b5da06b07dc43d814c6758291917346d24
Jorge Reyes <jorgeart81dev@gmail.com>
2025-09-17 19:09:03 -0500

    feat: Add calculatorKeys.ts

---
+++ src/app/calculator/services/calculatorKeys.ts
@@ -0,0 +1,33 @@
+export type CalculatorDecimalSeparator = '.' | ',';
+export type CalculatorOperator = 'ADD' | 'SUBTRACT' | 'MULTIPLY' | 'DIVIDE' | 'EQUAL' | 'PERCENT' | 'NEGATE';
+export type CalculatorCommand = 'CLEAR' | 'DELETE';
+
+export type CalculatorAction =
+  | CalculatorDecimalSeparator
+  | CalculatorOperator
+  | CalculatorCommand;
+
+export const calculatorAllowedKeys: Record<string, CalculatorAction | number> = {
+  'Escape': 'CLEAR',
+  'Clear': 'CLEAR',
+  '%': 'PERCENT',
+  '/': 'DIVIDE',
+  '*': 'MULTIPLY',
+  '-': 'SUBTRACT',
+  '+': 'ADD',
+  'Enter': 'EQUAL',
+  '.': '.',
+  '±': 'NEGATE',
+  'Backspace': 'DELETE',
+  'Delete': 'DELETE',
+  '0': 0,
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+};
