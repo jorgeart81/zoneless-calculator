a49ba83cf4c887bebe3861bf17aa1c8e417cdc5d
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-09-22 23:55:19 -0500

    router outlet test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,18 +1,27 @@
 import { provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
       providers: [provideZonelessChangeDetection()]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    compiled = fixture.nativeElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should render router outlet', () => {
+    expect(compiled.querySelector('router-outlet')).not.toBeNull()
   });
 });
