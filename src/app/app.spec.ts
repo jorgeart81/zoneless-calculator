3404efad3ca05fe7c47b7036d0fb69277c597dac
Jorge Reyes <jorgeart81dev@gmail.com>
2026-02-25 12:39:18 -0500

    improve app.spec.ts

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -21,7 +21,32 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render router outlet', () => {
+  it('should render router-outlet', () => {
     expect(compiled.querySelector('router-outlet')).not.toBeNull()
   });
+
+  it('should render router-outlet wrapper with css classes', () => {
+    const divElement = compiled.querySelector('div')
+    const mustHaveClasses = "min-w-screen min-h-screen bg-gray-600 flex items-center justify-center px-5 py-5".split(' ')
+
+    expect(divElement).not.toBeNull()
+    // divElement?.classList.forEach(className => expect(mustHaveClasses).toContain(className))
+
+    const divClasses = divElement?.classList.value.split(' ')
+    mustHaveClasses?.forEach(className => expect(divClasses).toContain(className))
+  });
+
+  it("should contain the 'buy me a beer' link", () => {
+    const anchorElement = compiled.querySelector('a')
+    const title = "Buy me a beer"
+    const link = "https://www.buymeacoffee.com/scottwindon"
+
+    expect(anchorElement).not.toBeNull()
+    expect(anchorElement?.title).toBe(title)
+
+    expect(anchorElement?.href).not.toBe(window.location.href)
+    expect(anchorElement?.href.trim().length).toBeGreaterThan(0)
+    expect(anchorElement?.href).toBe(link)
+  });
+
 });
